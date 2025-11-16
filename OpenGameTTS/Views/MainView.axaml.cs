55f846072bfbce74c47841cdb6f33d9d018caf44
kazaxc <lgkscast@gmail.com>
2025-11-16 02:14:22 +0000

    Setup blank Avalonia project structure

---
+++ OpenGameTTS/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace OpenGameTTS.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OpenGameTTS/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace OpenGameTTS.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
